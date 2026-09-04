90ae033f1f4e1c7f4826751d11676724b4364faa
soitamulati <jakimoremulati@gmail.com>
2026-09-04 08:19:40 -0700

    Configure build directory and set Java/Kotlin versions
    Updated build directory configuration and ensured Java and Kotlin compatibility with version 17.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,18 +5,29 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    afterEvaluate {
+        // Fix Java version
+        extensions.findByType<com.android.build.gradle.BaseExtension>()?.let { ext ->
+            ext.compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+        // Fix Kotlin version - THIS fixes your error
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            jvmTarget = "17"
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
